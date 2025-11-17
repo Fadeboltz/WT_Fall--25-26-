026427f02329d619530cbd69d8d5f11eb20efdac
Fadeboltz <nabilmohammednasim@gmail.com>
2025-11-17 11:38:21 +0600

    added more space

--- Midterm/labtask2/lab2.php
+++ Midterm/labtask2/lab2.php
@@ -19,7 +19,7 @@
     .formpage
     {
         background-color: #ffffff;
-        width: 350px;
+        width: 500px;
         padding: 20px;
         margin: auto;
     }
@@ -27,17 +27,19 @@
 <body>
 
     
-        <h1>Participant Registration Form</h1>
-   
-    Full Name: <br>
-    <input type="text" /> <br>
+     <h1>Participant Registration Form</h1>
+    <div class="formpage">
+    Full Name: <br> 
+    <input type="text" /> <br> <br>
     Email:  <br>
-    <input type="text" /> <br>
+    <input type="text" /> <br> <br>
     Phone Number:  <br>
-    <input type="number" /> <br>
+    <input type="number" /> <br> <br>
     Password :  <br>
-    <input type="password" /> <br>
+    <input type="password" /> <br> <br>
     Confirm Password:  <br>
     <input type="password" /> <br><br>
 
     <input type="submit" value="Register">
+    </div>
+
