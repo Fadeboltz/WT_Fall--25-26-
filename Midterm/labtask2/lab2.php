00a9286bd2d0b77672c2b3d8713bb5436d84b3f9
Fadeboltz <nabilmohammednasim@gmail.com>
2025-11-17 11:17:19 +0600

    updated with more spaces

--- Midterm/labtask2/lab2.php
+++ Midterm/labtask2/lab2.php
@@ -11,4 +11,15 @@
         <h1>Participant Registration Form</h1>
     </center>
 
-    
+    Full Name: <br>
+    <input type="text" /> <br>
+    Email:  <br>
+    <input type="text" /> <br>
+    Phone Number:  <br>
+    <input type="number" /> <br>
+    Password :  <br>
+    <input type="password" /> <br>
+    Confirm Password:  <br>
+    <input type="password" /> <br><br>
+
+    <input type="submit" value="Register">
